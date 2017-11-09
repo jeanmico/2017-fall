313d7dab67de74dae239ca38a19ce9e4e43192aa
jeanmico <32104713+jeanmico@users.noreply.github.com>
2017-11-09 15:48:31 -0800

    Update set_regex.py
    add negative set, fix issue with '.' in regex

--- set_regex.py
+++ set_regex.py
@@ -10,20 +10,25 @@ path = os.path.join(os.path.sep, 'Users', 'student', 'pollard',
 pos_fname = "set_positive.csv"
 neg_fname = "set_negative.csv"
 
-pos = []
-neg = []
+expressions = []
 with open(os.path.join(path, pos_fname)) as posfile:
 	# read in the positive sample and bin ids
 	for line in posfile.readlines()[1:]:
 		temp = line.strip().split(',')
-		regstr = temp[0] + "[[:blank:]]" + temp[1]  # TODO: fix the . in the regex pattern
-		pos.append(regstr)
-		# #grepstr = 'grep --color=never ' + regstr + ' ' + os.path.join(path, fname) + ' >> out.txt'
-		# grepstr = ['grep', '--color=never', 'regstr', os.path.join(path, fname), '>>',  'out.txt']
-		# call(grepstr, shell=True)
+		bin_id = temp[1].split('.')
+		regstr = temp[0] + "[[:blank:]]" + bin_id[0] + '\.' + bin_id[1]
+		expressions.append(regstr)
 
-with open(os.path.join(path, "pos_pattern.txt"), "w+") as output:
-	for item in pos:
+with open(os.path.join(path, neg_fname)) as negfile:
+	# read in the positive sample and bin ids
+	for line in negfile.readlines()[1:]:
+		temp = line.strip().split(',')
+		bin_id = temp[1].split('.')
+		regstr = temp[0] + "[[:blank:]]" + bin_id[0] + '\.' + bin_id[1]
+		expressions.append(regstr)
+
+with open(os.path.join(path, "set_pattern.txt"), "w+") as output:
+	for item in expressions:
 		output.write("%s\n" % item)
 
 # RUN THE FOLLOWING COMMAND:
